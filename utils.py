b807ca65c9fe491da209be7be33fc8cfcc17baa1
David <avatar_4500@mail.ru>
2021-11-07 02:56:24 +0300

    dropped deprecated functions

--- utils.py
+++ utils.py
@@ -260,7 +260,7 @@ class grainFig():
 
         for i, left_point in enumerate(side):
             right_point = (left_point[0] + l1), (left_point[1] + l2)
-            line_points = cls.get_line(left_point, right_point)
+            line_points = cls.line(left_point, right_point)
             for j, point in enumerate(line_points):
                 lines[i, j] = point
 
@@ -268,11 +268,25 @@ class grainFig():
 
 
 class grainMark():
+    @classmethod
+    def mark_corners_and_classes(cls, image, max_num=100000, sens=0.1, max_dist=1):
+        #
+        # НЕТ ГАРАНТИИ РАБОТЫ
+        #
+        corners = cv2.goodFeaturesToTrack(image, max_num, sens, max_dist)
+        corners = np.int0(corners)
+        x = copy.copy(corners[:, 0, 1])
+        y = copy.copy(corners[:, 0, 0])
+        corners[:, 0, 0], corners[:, 0, 1] = x, y
+
+        classes = filters.rank.gradient(image, disk(1)) < 250
+        classes, num = ndi.label(classes)
+        return corners, classes, num
+
 
     @classmethod
     def mean_pixel(cls, image, point1, point2, r):
 
-        val1, num1 = cls.draw_rect(image, point1, point2, r)
         val2, num2 = cls.draw_rect(image, point2, point1, r)
         val = val1 + val2
         num = num1 + num2
@@ -488,6 +502,18 @@ class grainShow():
 
 
 class grainDraw():
+    @classmethod
+    def draw_corners(cls, image, corners, color=255):
+        #
+        # НЕТ ГАРАНТИИ РАБОТЫ
+        #
+        image = copy.copy(image)
+        for i in corners:
+            x, y = i.ravel()
+            cv2.circle(image, (x, y), 3, color, -1)
+
+        return image
+
 
     @classmethod
     def draw_edges(cls, image, cnts, color=(50, 50, 50)):
@@ -797,327 +823,3 @@ class grainGenerate():
             np.save(f'{folder}/texts_step_{step}.npy', np.array(texts))
 
 
-class grainDeprecated():
-    @classmethod
-    def mark_corners_and_classes(cls, image, max_num=100000, sens=0.1, max_dist=1):
-        #
-        # НЕТ ГАРАНТИИ РАБОТЫ
-        #
-        corners = cv2.goodFeaturesToTrack(image, max_num, sens, max_dist)
-        corners = np.int0(corners)
-        x = copy.copy(corners[:, 0, 1])
-        y = copy.copy(corners[:, 0, 0])
-        corners[:, 0, 0], corners[:, 0, 1] = x, y
-
-        classes = filters.rank.gradient(image, disk(1)) < 250
-        classes, num = ndi.label(classes)
-        return corners, classes, num
-
-    @classmethod
-    def get_nodes_corners_classes_classesNum(cls,
-                                             image,
-                                             max_corners_per_class,
-                                             max_num=100000,
-                                             sens=0.1,
-                                             max_dist=1):
-        #
-        # НЕТ ГАРАНТИИ РАБОТЫ
-        #
-        corners, classes, classes_num = cls.mark_corners_and_classes(image, max_num, sens, max_dist)
-        shape = (classes_num + 1, max_corners_per_class)
-        nodes = np.zeros(shape, dtype='int64')
-        r = 2
-        for i, corner in enumerate(corners):
-            x = corner[0][0]
-            y = corner[0][1]
-            cls = classes[x, y]
-            real_cls = 0
-            flag = True
-            x_lin = range(x - r, x + r)
-            y_lin = range(y - r, y + r)
-            while flag:
-                for xi in range(x - r, x + r):
-                    for yi in y_lin:
-                        if classes[xi, yi] != 0 and classes[xi, yi] != 1 and classes[xi, yi] != 2:
-                            real_cls = classes[xi, yi]
-                            flag = False
-                        elif yi == y_lin[-1] and xi == x_lin[-1]:
-                            flag = False
-
-            index = np.where(nodes[real_cls] == 0)[0][0]
-            nodes[real_cls, index] = i
-            nodes[real_cls, -1] += 1
-
-        return nodes, classes, classes_num, corners
-
-    @classmethod
-    def estimate_edges(cls, image, node, corners, position=0, radius=2):
-        #
-        # НЕТ ГАРАНТИИ РАБОТЫ
-        #
-
-        # вычисление расстояния и среднего значения между
-        # точкой с индексом posion и остальными точками пустоты
-        v1 = node[position]
-        vals = np.zeros((len(node), 2))
-        if v1 != 0:
-            for i, v2 in enumerate(node):
-
-                if v2 != 0 and v1 != v2:
-                    vals[i, 0] = v2
-                    point1 = corners[v1]
-                    point2 = corners[v2]
-                    mean, dist = cls.mean_pixel(image, point1[0], point2[0], radius)
-                    vals[i, 1] = abs(mean - 0.5) * dist
-            #     vals[i,2]=dist
-
-        #  vals=vals[np.argsort(vals[:,2])]
-        return vals
-
-    @classmethod
-    def sort_perimetr(cls, orig_image, nodes, corners, eps, radius):
-        #
-        # НЕТ ГАРАНТИИ РАБОТЫ
-        #
-        img = Image.fromarray(orig_image)
-        img = ImageOps.expand(img, border=radius, fill='black')
-        image = np.array(img)
-
-        new_nodes = np.zeros(nodes.shape, dtype='int64')
-
-        # проходим по каждой пустоте
-        for i, orig_node in enumerate(nodes):
-            # node - пустота
-            v1_index = 0
-            v1 = orig_node[v1_index]  # фиксируем первый угол и затем будем считать относительно него
-            step = 1
-            new_nodes[i, 0] = v1
-            flag = True
-            orig_node_len = orig_node[-1]
-            node = list(copy.copy(orig_node[:orig_node_len]))
-            # norm_val=mean_dist_points(node,corners)
-            #  print('i=',i, ' max=',nodes.shape[0])
-            #  print('-------------')
-            # print('old_node',node)
-            # print('new_node')
-            if v1 != 0 and len(node) > 1:
-                while flag:
-                    #        print('delete_node,iteration start',node)
-                    #       print('v1',v1)
-                    vals = cls.estimate_edges(image, node, corners, position=v1_index, radius=radius)
-                    non_zero_indeces = np.where((vals[:, 0][:orig_node_len] != 0))[0]
-                    vals = vals[non_zero_indeces]
-                    #    print('new kernel point')
-
-                    min_val = vals.min(axis=0)[1]
-                    #         for j,val in enumerate(vals):
-                    #               mean=val[1]
-                    #              if abs(mean-0.5)<=eps:
-                    #                v2=val[0]
-                    #                break
-                    #            elif j==vals.shape[0]-1:
-                    #              v2=val[0]
-                    #               break
-
-                    #  print('min_val',min_val)
-                    v2_vals_index = np.where(vals[:, 1] == min_val)[0][0]
-                    v2 = vals[v2_vals_index][0]
-                    #     print('v2',v2)
-
-                    new_nodes[i, step] = v2
-                    node.pop(v1_index)
-
-                    v1 = v2
-                    v2_index = node.index(v2)
-
-                    #    print('v2_index',v2_index)
-                    v1_index = v2_index
-
-                    #     if len(node)==1:
-                    #           new_nodes[i,step+1]=node[0]
-                    #           flag=False
-
-                    step += 1
-                    if step == orig_node_len:
-                        flag = False
-
-            else:
-                continue
-            new_nodes[i, -1] = orig_node_len
-        return new_nodes
-
-    @classmethod
-    def sor_perimetr_hull(cls, image, nodes, corners, color=(51, 51, 51)):
-        #
-        # НЕТ ГАРАНТИИ РАБОТЫ
-        #
-        new_nodes = np.zeros(nodes.shape, dtype='int64')
-
-        for i, node in enumerate(nodes):
-
-            if node[0] != 0 and node[-1] > 2:
-                l = node[-1]
-                points = np.zeros((l, 2))
-                for j, point in enumerate(node[:l]):
-                    points[j] = corners[point, 0]
-
-                hull = ConvexHull(points)
-                for simplex in hull.simplices:
-                    (x1, x2) = points[simplex, 0]
-                    (y1, y2) = points[simplex, 1]
-
-        for j, node in enumerate(nodes):
-            if len(node) > 1:
-                r = 3
-                len_node = node[-1]
-
-                for i, point in enumerate(node[: len_node]):
-                    point2 = corners[point][0]
-                    x2, y2 = point2[0], point2[1]
-
-            else:
-                continue
-
-        return image
-
-    @classmethod
-    def corners_classes(cls, nodes, classes, max_num=2000, size=5, show=False):
-        #
-        # НЕТ ГАРАНТИИ РАБОТЫ
-        #
-        node_corner_numbers = np.zeros((classes.shape[0], 1))
-        for i, node in enumerate(nodes):
-            for corner in node:
-                if corner:
-                    node_corner_numbers[i] += 1
-
-        std = node_corner_numbers.std(axis=0)[0]
-        mean = node_corner_numbers.mean(axis=0)[0]
-
-        print('std: ', round(std, 3))
-        print('mean: ', round(mean, 3))
-        print('количесвто углов: ', classes.shape[0])
-        if show:
-            fig, ax = plt.subplots(figsize=(size, size))
-            ax.set_ylabel('Количество углов у пустоты', size=20)
-            ax.set_xlabel('Порядковый номер пустоты', size=20)
-            plt.plot(node_corner_numbers[:max_num])
-        return node_corner_numbers
-
-    @classmethod
-    def classes_corners(cls, nodes, corners, max_num=2000, size=5, show=False):
-        #
-        # НЕТ ГАРАНТИИ РАБОТЫ
-        #
-        corner_distr = np.zeros((corners.shape[0]))
-
-        for i in range(corners.shape[0]):
-            for node in nodes:
-                node_len = node[-1]
-                if i in node[:node_len]:
-                    corner_distr[i] += 1
-
-        std = corner_distr.std(axis=0)
-        mean = corner_distr.mean(axis=0)
-
-        print('std: ', round(std, 3))
-        print('mean: ', round(mean, 3))
-        print('количество пустот: ', nodes.shape[0])
-        if show:
-            fig, ax = plt.subplots(figsize=(size, size))
-            ax.set_ylabel('Количество вхождений в пустоту', size=20)
-            ax.set_xlabel('Порядковый номер угла', size=20)
-            plt.plot(corner_distr[:max_num])
-            plt.show()
-        return corner_distr
-
-
-    @classmethod
-    def draw_corners(cls, image, corners, color=255):
-        #
-        # НЕТ ГАРАНТИИ РАБОТЫ
-        #
-        image = copy.copy(image)
-        for i in corners:
-            x, y = i.ravel()
-            cv2.circle(image, (x, y), 3, color, -1)
-
-        return image
-
-
-    @classmethod
-    def draw_edges_nodes(cls, image, nodes, corners, color=(51, 51, 51)):
-        #
-        # НЕТ ГАРАНТИИ РАБОТЫ
-        #
-        new_image = copy.copy(image)
-        im = Image.fromarray(np.uint8(cm.gist_earth(new_image) * 255))
-        draw = ImageDraw.Draw(im)
-        for j, node in enumerate(nodes):
-            if len(node) > 1:
-                #   print('i=',j)
-                #    print(node)
-                point1 = corners[node[0]][0]
-                x1, y1 = point1[0], point1[1]
-
-                x_start, y_start = point1[0], point1[1]
-                r1 = 5
-                r = 3
-                draw.ellipse((y1 - r1, x1 - r1, y1 + r1, x1 + r1), fill=color, width=10)
-                len_node = node[-1]
-                #     print(node[:len_node])
-                for i, point in enumerate(node[: len_node]):
-                    point2 = corners[point][0]
-                    x2, y2 = point2[0], point2[1]
-
-                    draw.ellipse((y2 - r, x2 - r, y2 + r, x2 + r), fill=color, width=4)
-                    draw.line((y1, x1, y2, x2), fill=color, width=4)
-                    x1, y1 = x2, y2
-
-                draw.line((y_start, x_start, y1, x1), fill=(100, 100, 100), width=4)
-            else:
-                continue
-
-        img = np.array(im)
-
-        return img
-
-    @classmethod
-    def draw_edges_hull(cls, image, nodes, corners, color=(51, 51, 51)):
-        #
-        # НЕТ ГАРАНТИИ РАБОТЫ
-        #
-        new_image = copy.copy(image)
-        im = Image.fromarray(np.uint8(cm.gist_earth(new_image) * 255))
-        draw = ImageDraw.Draw(im)
-
-        for i, node in enumerate(nodes):
-
-            if node[0] != 0 and node[-1] > 2:
-                l = node[-1]
-                points = np.zeros((l, 2))
-                for j, point in enumerate(node[:l]):
-                    points[j] = corners[point, 0]
-
-                hull = ConvexHull(points)
-                for simplex in hull.simplices:
-                    (x1, x2) = points[simplex, 0]
-                    (y1, y2) = points[simplex, 1]
-
-                    points[simplex, 0], points[simplex, 1]
-                    draw.line((y1, x1, y2, x2), fill=(100, 100, 100), width=4)
-        for j, node in enumerate(nodes):
-            if len(node) > 1:
-                r = 3
-                len_node = node[-1]
-
-                for i, point in enumerate(node[: len_node]):
-                    point2 = corners[point][0]
-                    x2, y2 = point2[0], point2[1]
-                    draw.ellipse((y2 - r, x2 - r, y2 + r, x2 + r), fill=color, width=4)
-            else:
-                continue
-
-        img = np.array(im)
-
-        return img
