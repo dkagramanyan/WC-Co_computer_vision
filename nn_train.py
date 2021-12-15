33472d0e4c4ddec4bbb74e939e1d91c76986929a
David <avatar_4500@mail.ru>
2021-12-15 14:42:43 +0300

    refactor

--- nn_train.py
+++ nn_train.py
@@ -1,4 +1,4 @@
-from u2net import U2NET
+from src.u2net import U2NET
 import numpy as np
 import datetime
 import tensorflow as tf
@@ -7,27 +7,7 @@ from sklearn.model_selection import train_test_split
 import csv
 import os
 
-
-class SaveCallback(tf.keras.callbacks.Callback):
-
-    def __init__(self, test_images, save_dir, n_samples, net):
-        super().__init__()
-        self.images = test_images
-        self.safe_dir = save_dir
-        self.n_samples = n_samples
-        self.net = net
-
-        if not os.path.exists(save_dir):
-            os.makedirs(self.safe_dir)
-
-    def on_epoch_end(self, epoch, logs=None):
-        indeces = np.random.randint(0, len(self.images), size=self.n_samples)
-        for i in indeces:
-            predict = self.net(tf.expand_dims(self.images[i], axis=0))[0][0]
-            original = self.images[i]
-            concat = np.concatenate([predict.numpy(), original], axis=1)
-            tf.keras.preprocessing.image.save_img(f'{self.safe_dir}epoch={epoch}_index={i}.png', concat * 255)
-
+from src.nn_utils import SaveImageCallback
 
 all_images = np.load('data/saved np/all_images_no_preprocess.npy', allow_pickle=True)
 all_images_rgb = []
@@ -82,7 +62,7 @@ csv_logger = tf.keras.callbacks.CSVLogger(csv_log_filepath)
 
 n_samples = 3
 
-save_callback = SaveCallback(x_test, images_save_dir, n_samples, net)
+save_callback = SaveImageCallback(x_test, images_save_dir, n_samples, net)
 
 model.compile(optimizer=optim, loss='mse', metrics=['MAE'])
 history = model.fit(x_train, y_train,
